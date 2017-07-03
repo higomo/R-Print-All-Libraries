c3ffa0950b8f6b373e92c2766826e368527b2fa5
higomo <cleevno@gmail.com>
2017-07-03 16:06:57 -0700

    Add files via upload

--- LibraryExport.R
+++ LibraryExport.R
@@ -1,4 +1,5 @@
 #code to import all available libraries
+library(plyr);library(rstudioapi);sendToConsole({
 x<-"";for(num in 1:nrow(installed.packages())-1){x <- paste(x,paste0("library(",(installed.packages()[num,1]),");"))}
 (x <- paste(x,paste0("library(",(installed.packages()[nrow(installed.packages()),1]),")")))
-library(rstudioapi);sendToConsole(x)
+})
