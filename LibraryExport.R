fc39463e497ced1697521ead8e9f1a85b4ee06f2
higomo <cleevno@gmail.com>
2017-07-03 12:18:39 -0700

    Add files via upload

--- LibraryExport.R
+++ LibraryExport.R
@@ -1,4 +1,3 @@
 #code to import all libraries, 1 line of code; copy from console
 x<-"";for(num in 1:nrow(installed.packages())-1){x <- paste(x,paste0("library(",(installed.packages()[num,1]),");"))}
-x <- paste(x,paste0("library(",(installed.packages()[nrow(installed.packages()),1]),")"))
-x
+(x <- paste(x,paste0("library(",(installed.packages()[nrow(installed.packages()),1]),")")))
